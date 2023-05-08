6185a626aeaaf5e891dfef70074c914b654c7775
Anthony Salemo <ajssalemo@gmail.com>
2023-05-08 13:37:35 -0400

    having a working local implementation of a config client

--- client/src/main/java/com/springapps/azure/Controllers/ConfigClientController.java
+++ client/src/main/java/com/springapps/azure/Controllers/ConfigClientController.java
@@ -3,12 +3,10 @@ package com.springapps.azure.Controllers;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @RestController
-@RefreshScope
 public class ConfigClientController {
-    @Value("${azure.name}")
+    @Value("${cloud.name}")
     private String cloudName;
 
     public String azureSpringAppsApplicationName = "Azure Spring App Name: " + this.cloudName;
