357ffe5018b4550cf2b82ec7b7727486bdab2f5c
Anthony Salemo <ajssalemo@gmail.com>
2024-01-01 14:15:19 -0500

    changing config

--- client/src/main/java/com/springapps/azure/Controllers/ConfigClientController.java
+++ client/src/main/java/com/springapps/azure/Controllers/ConfigClientController.java
@@ -5,8 +5,8 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RefreshScope
 @RestController
+@RefreshScope
 public class ConfigClientController {
     @Value("${cloud.name}")
     private String cloudName;
