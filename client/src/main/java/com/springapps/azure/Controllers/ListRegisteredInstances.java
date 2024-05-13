df89698cb8c8bec03b04e69d95a6ebbb1511b948
Anthony Salemo <ajssalemo@gmail.com>
2024-05-13 12:29:38 -0400

    updated how to call eureka server for registered instances

--- client/src/main/java/com/springapps/azure/Controllers/ListRegisteredInstances.java
+++ client/src/main/java/com/springapps/azure/Controllers/ListRegisteredInstances.java
@@ -1,20 +1,30 @@
 package com.springapps.azure.Controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class ListRegisteredInstances {
-    @GetMapping("/api/eureka/get/instances")
-    public String apiController() {
-        final String uri = "http://localhost:8761/eureka/apps";
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
-        RestTemplate restTemplate = new RestTemplate();
-        String result = restTemplate.getForObject(uri, String.class);
+    @GetMapping(value = "/api/eureka/get/instances", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<ServiceInstance> listRegisteredInstances() {
 
-        System.out.println(result);
-
-        return result;
+        List<String> services = discoveryClient.getServices();
+        List<ServiceInstance> instances = new ArrayList<ServiceInstance>();
+        services.forEach(serviceName -> {
+            discoveryClient.getInstances(serviceName).forEach(instance -> {
+                instances.add(instance);
+            });
+        });
+        return instances;
     }
 }
